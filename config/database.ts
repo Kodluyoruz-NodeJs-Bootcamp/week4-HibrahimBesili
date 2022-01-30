1729cb10381b319b1ccc68b4e590226005225fa6
HibrahimBesili <hibesili533@icloud.com>
2022-01-31 00:53:49 +0300

    AuthApp updated.

--- config/database.ts
+++ config/database.ts
@@ -1,29 +1,3 @@
-// import { OperationCanceledException } from "typescript";
-
-// // import config from '../config.json';
-// import  mySql from "mysql";
-
-// mySql.createConnection()
-
-// var connectDB = mySql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "12345",
-//   database : "sys"
-// });
-
-// // connectDB.connect(
-// //   function (err) { 
-// //   if (err) { 
-// //       console.log("!!! Cannot connect !!! Error:");
-// //       throw err;
-// //   }
-// //   else
-// //   {
-// //      console.log("Connection established.");
-// //   }
-// // });
-// export default connectDB;
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 
